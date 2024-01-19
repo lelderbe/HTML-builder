a3fdff0fea4d620bfc30a596d99f69a75f1e8444
lelderbe <fvdc@ya.ru>
2024-01-19 10:42:41 +0300

    feat: add solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const writeableStream = fs.createWriteStream(
+  path.join(__dirname, 'text.txt'),
+  'utf8',
+);
+
+stdin.on('data', (data) => {
+  if (data.toString() === 'exit\n') {
+    process.exit();
+  }
+  writeableStream.write(data);
+});
+
+stdout.write('Привет! Напиши что-нибудь и я сохраню это в файл:\n');
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+process.on('exit', () => stdout.write('Goodbye!\n'));
