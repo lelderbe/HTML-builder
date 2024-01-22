90a9016851c4663dff6ca679aba74e49469c3317
lelderbe <fvdc@ya.ru>
2024-01-22 22:39:47 +0300

    fix: change streams to read/writeFile methods

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,34 +1,37 @@
-const fs = require('fs');
-const fsPromises = require('fs/promises');
+const fs = require('fs/promises');
 const path = require('path');
 
-const writeableStream = fs.createWriteStream(
-  path.join(__dirname, 'project-dist', 'bundle.css'),
-  'utf8',
-);
-
-async function makeBundle() {
-  const targetFolder = path.join(__dirname, 'styles');
+async function mergeStyles() {
+  const srcFolder = path.join(__dirname, 'styles');
+  const destFolder = path.join(__dirname, 'project-dist');
 
   try {
-    const files = await fsPromises.readdir(targetFolder, {
+    const files = await fs.readdir(srcFolder, {
       withFileTypes: true,
     });
-    files.forEach(async (file) => {
-      if (file.isFile()) {
-        const ext = path.parse(file.name).ext;
-        if (ext === '.css') {
-          const readableStream = fs.createReadStream(
-            path.join(__dirname, 'styles', file.name),
-            'utf8',
-          );
-          readableStream.on('data', (chunk) => writeableStream.write(chunk));
+
+    const promises = files
+      .filter((file) => {
+        if (!file.isFile()) {
+          return false;
         }
-      }
-    });
+        const ext = path.parse(file.name).ext;
+        if (ext !== '.css') {
+          return false;
+        }
+        return true;
+      })
+      .map((file) => {
+        return fs.readFile(path.join(srcFolder, file.name), {
+          encoding: 'utf8',
+        });
+      });
+    const result = await Promise.all(promises);
+    await fs.writeFile(path.join(destFolder, 'bundle.css'), result.join('\n'));
   } catch (err) {
+    console.error('Error: cannot read styles folder');
     return;
   }
 }
 
-makeBundle();
+mergeStyles();
